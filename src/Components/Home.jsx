b8278a2806f0773095fe7da9a2bcbdbccf04a235
FackJox <jackfox@tuta.io>
2023-10-02 12:20:18 +0100

    polish

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -323,8 +323,9 @@ export const Home = () => {
           </h1>
        
           
-            
-          <div className="absolute top-[585vh] w-screen h-screen flex items-center justify-center align-center">
+          <div className="absolute top-[585vh] w-screen flex-col items-center justify-center align-center">
+
+          <div className="relative flex h-screen justify-center items-center align-center">
             <video
               src={video}
               ref={videoElement}
@@ -333,7 +334,7 @@ export const Home = () => {
               loop
               muted
               playsInline
-              className="flex object-cover p-2 m-2 md:p-5 md:m-5 w-full h-3/4  items-center justify-center"
+              className="flex object-cover p-2 m-2 md:p-5 md:m-5 w-full h-3/4 items-center justify-center"
               onClick={() => {
                 if (videoElement.current) {
                   videoElement.current.requestFullscreen();
@@ -341,13 +342,13 @@ export const Home = () => {
               }}
             />
             </div>
-          <div className="absolute top-[673vh] w-screen h-screen flex ">
-          <div className=" p-2 m-2 md:p-5 md:m-5 w-full h-3/4 md:h-screen flex ">
+          <div className="relative w-screen h-screen flex  mt-[-100px]">
+          <div className=" p-2 m-2 md:p-5 md:m-5 md:mt-[0px] w-full h-3/4 md:h-screen flex ">
 
             <iframe src="https://my.atlist.com/map/2535defd-50df-4387-b379-06d89c4a1d14?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="100%"  allowFullScreen></iframe>
             </div>
             </div>
-          
+          </div>
         </Scroll>
       </ScrollControls>
     </Canvas>
